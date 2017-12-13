2b91af1541fab39652b4d37d113b56b8cc759d5d
Gideon Oduro <gideon.oduro@tele2.com>
2017-12-13 14:47:11 +0100

    added clean up plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require('path')
+    , CleanWebpackPlugin = require('clean-webpack-plugin')
     , SRC = path.join(__dirname, './src')
     , ENTRY = path.join(SRC, './backend/index.ts')
     , DIST = Object.assign({path: path.join(SRC, './backend/dist'), filename: 'bundle.[hash].js'});
 
 function resolve() {
-    return { extensions: [".ts", ".tsx", ".js", ".jsx"]  }
+    return { extensions: [".ts", ".tsx", ".js"]  }
 }
 
 function rules() {
-    return [ { test: /\.tsx?$/, loader: 'ts-loader' } ]
+    return [ { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ } ];
+}
+
+function plugins() {
+    return [ new CleanWebpackPlugin(['dist']) ];
 }
 
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: ENTRY,
-    output: DIST,
-    resolve: resolve(),
-    module: { rules: rules() }
+    devtool: 'inline-source-map'
+    , entry: ENTRY
+    , output: DIST
+    , resolve: resolve()
+    , module: { rules: rules() }
+    , plugins: plugins()
 };
