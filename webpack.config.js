eef761265e866b17fd1a00d82450fdac33f49026
Gideon Oduro <gideon.oduro@tele2.com>
2017-12-13 14:38:32 +0100

    setup ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path')
     , SRC = path.join(__dirname, './src')
     , ENTRY = path.join(SRC, './backend/index.ts')
-    , DIST = Object.assign({path: path.join(SRC, './backend/dist'), filename: 'bundle.js'});
+    , DIST = Object.assign({path: path.join(SRC, './backend/dist'), filename: 'bundle.[hash].js'});
 
 function resolve() {
     return { extensions: [".ts", ".tsx", ".js", ".jsx"]  }
 }
 
+function rules() {
+    return [ { test: /\.tsx?$/, loader: 'ts-loader' } ]
+}
+
 module.exports = {
+    devtool: 'inline-source-map',
     entry: ENTRY,
     output: DIST,
-    resolve: resolve()
+    resolve: resolve(),
+    module: { rules: rules() }
 };
