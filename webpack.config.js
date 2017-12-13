f1e377752b036377ee468ddb35b2ccb0cf93df65
Gideon Oduro <gideon.oduro@tele2.com>
2017-12-13 13:48:11 +0100

    initail typscript, webpack, npm script setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+    , SRC = path.join(__dirname, './src')
+    , ENTRY = path.join(SRC, './backend/index.ts')
+    , DIST = Object.assign({path: path.join(SRC, './backend/dist'), filename: 'bundle.js'});
+
+function resolve() {
+    return {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    }
+}
+
+module.exports = {
+    entry: ENTRY,
+    output: DIST,
+    resolve: resolve()
+};
