7c1e684dd2a4ab5283193de648130105fc414720
Gideon Oduro <gideon.oduro@tele2.com>
2017-12-30 18:54:53 +0100

    introduced builder pattern to socket client

--- src/frontend/communication/socket-client.js
+++ src/frontend/communication/socket-client.js
@@ -1,9 +1,70 @@
-const url = `ws://localhost:8080/`;
-const io = new WebSocket(url);
-io.onopen = () => console.log(`established connection to: ${url}`);
-// TODO on message need to handle messages with different return type to avoid error message
-io.onmessage = ({data}) => (document.querySelector('img').src = URL.createObjectURL(data));
-io.onclose = (e) => console.log(e);
-io.onerror = (err) => console.log(err);
+const defaultURL = `ws://localhost:8080/`;
 
-export default io;
+export class Socket {
+    constructor(build) {
+        this.socket = build
+    }
+    static get IO() {
+        class IO {
+            constructor(url = defaultURL) {
+                this.socket = new WebSocket(url);
+            }
+            setOnOpen(open) {
+                this.socket.onopen = open;
+                return this
+            }
+            setOnMessage(message) {
+                this.socket.onmessage = message;
+                return this
+            }
+            setOnClose(close) {
+                this.socket.onclose = close;
+                return this
+            }
+            setOnError(error) {
+                this.socket.onerror = error;
+                return this
+            }
+            build() {
+                return this.socket;
+            }
+        }
+        return IO;
+    }
+}
+
+export const SimpleSocket = (open = onopen, message = onmessage) => {
+    return new Socket.IO()
+        .setOnOpen(open)
+        .setOnMessage(message)
+        .setOnClose(onclose)
+        .setOnError(onerror)
+        .build();
+};
+
+const onopen = () => console.log(`established connection to: ${defaultURL}`);
+const onmessage = ({data}) => handleData(data);
+const onclose = (e) => console.log(e);
+const onerror = (err) => console.log(err);
+
+const handleData = (data) => {
+    if(data instanceof Blob){
+        document.querySelector('img').src = URL.createObjectURL(data);
+    }
+    if(data instanceof String) {
+        console.log(data)
+    }
+
+    //TODO fix switch statement
+    // switch(data) {
+    //     case data instanceof Blob:
+    //
+    //          document.querySelector('img').src = URL.createObjectURL(data);
+    //         break;
+    //     case data instanceof String:
+    //         console.log(data);
+    //         break;
+    //     default:
+    //         console.log('unhandled data type');
+    // }
+};
--- src/frontend/component/VideoCapture.js
+++ src/frontend/component/VideoCapture.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import RecordBnt from './RecordBtn'
 import {start, stop} from "../service/capture";
 import {WIDTH, HEIGHT} from "../utility/constants";
-import io from '../communication/socket-client'
+import {SimpleSocket} from '../communication/socket-client'
 import {drawImage} from "../utility/canvas-support";
-const send = (_) => drawImage(document.querySelector('#camera-stream'))((data) => io.send(data));
-export const VideoCapture = ({}) =>
 
+
+const io = SimpleSocket();
+const send = (_) => drawImage(document.querySelector('#camera-stream'))((data) => io.send(data));
+
+
+
+export const VideoCapture = ({}) =>
     /**
      * TODO use ref callback function to access dom element
      * <video id='camera-stream'
