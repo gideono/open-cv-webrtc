10d4ace3c5b96753bfe47a2934629782f50a517f
Gideon Oduro <gideon.oduro@tele2.com>
2018-01-01 14:24:54 +0100

    fixed animation on notification bar and othere stuff

--- src/frontend/component/notification.js
+++ src/frontend/component/notification.js
@@ -3,18 +3,3 @@ export default ({children, type, duration}) =>
     <section id="notification-bar" className={`${type}`}>
      {children}
     </section>
-
-
-// function createComponentWithDefaultProps(WrappedComponent, defaultProps) {
-//     return (props) => (
-//         <WrappedComponent {...defaultProps} {...props} />
-//     );
-// }
-//
-// const Layout = ({ children, theme }) => (
-//     <div className={`theme-${theme}`}>
-//         <main>{children}</main>
-//     </div>
-// );
-//
-// const DarkLayout = createComponentWithDefaultProps(Layout, { theme: 'dark' });
--- src/frontend/component/warning-notification.js
+++ src/frontend/component/warning-notification.js
@@ -3,5 +3,7 @@ import Notification from './notification'
 
 export const WarningNotification = () =>
     <Notification type='warning-notification' duration={5}>
-        <div>Oh God !!! something went horribly wrong</div>
+        <div>
+            <h4>oh God something went horribly wrong !!!!</h4>
+        </div>
     </Notification>;
