38b198a0c46589ba3394537bb4f159a8dcaa33cb
Gideon Oduro <gideon.oduro@tele2.com>
2018-01-03 21:04:07 +0100

    clean up css

--- src/frontend/component/record-btn.js
+++ src/frontend/component/record-btn.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const RecordBtn = ({props}) => [
     <input key="record-btn" type="checkbox" id="record-btn" name="cc" />,
     <label key="record-label" htmlFor="record-btn">
-        <div className="frame"><span></span></div>
+        <div><span></span></div>
     </label>
 ];
